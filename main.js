f163115b9f3f6b1e91f7abd9da12732abd275341
darlinng5 <darlinng5@gmail.com>
2024-08-29 10:38:40 -0600

    v1.0.2
    v1.0.2

--- main.js
+++ main.js
@@ -1,44 +1,45 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const url = require('url')
-const { updateElectronApp, UpdateSourceType } = require('update-electron-app')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { updateElectronApp, UpdateSourceType } = require('update-electron-app');
 
-
-let win
+let win;
 
 function createWindow() {
-  win = new BrowserWindow({ width: 1200, height: 800 })
+  win = new BrowserWindow({ width: 1200, height: 800 });
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'dist/inqura/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  let startUrl;
+  if (app.isPackaged) {
+    startUrl = new URL(path.join('file:', app.getAppPath(), 'dist', 'inqura', 'index.html')).href;
+  } else {
+    startUrl = new URL(path.join('file:', __dirname, 'dist', 'inqura', 'index.html')).href;
+  }
+
+  win.loadURL(startUrl);
 
   win.on('closed', () => {
-    win = null
-  })
+    win = null;
+  });
 }
+
 updateElectronApp({
-    updateSource: {
-      type: UpdateSourceType.ElectronPublicUpdateService,
-      repo: 'darlinng5/InquraCloud',
-      host:'https://update.electronjs.org'
-    },
-    updateInterval: '5 minutes',
-  })
+  updateSource: {
+    type: UpdateSourceType.ElectronPublicUpdateService,
+    repo: 'darlinng5/InquraCloud',
+    host: 'https://update.electronjs.org'
+  },
+  updateInterval: '5 minutes',
+});
 
-
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
